f30751d1f67877336d7c88ae12c46ea9fa4728ee
atha <giannisj3@gmail.com>
2024-06-21 16:26:14 +0300

    Add debouncing to search bar

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -3,13 +3,14 @@
 import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
 import { usePathname, useSearchParams } from 'next/navigation';
 import { useRouter } from 'next/navigation';
+import { useDebouncedCallback } from "use-debounce";
 
 export default function Search({ placeholder }: { placeholder: string }) {
   const pathname = usePathname();
   const searchParams = useSearchParams();
   const router = useRouter();
 
-  const handleSearch = (value: string) => {
+  const handleSearch = useDebouncedCallback((value: string) => {
     const params = new URLSearchParams(searchParams);
     if (value) {
       params.set('search', value);
@@ -18,7 +19,7 @@ export default function Search({ placeholder }: { placeholder: string }) {
     }
 
     router.replace(`${pathname}?${params.toString()}`);
-  };
+  }, 3000);
 
   return (
     <div className="relative flex flex-1 flex-shrink-0">
